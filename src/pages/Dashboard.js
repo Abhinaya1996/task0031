1e6c6f6f4109376be051c1e4ca6d257912c56e1e
Abhinaya1996 <164352445+Abhinaya1996@users.noreply.github.com>
2025-02-19 12:17:22 +0530

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -111,7 +111,7 @@ export default function Dashboard({selectedDate,selectedHotel}){
                                 <div className="card-body">
                                     <h2>Overview</h2>
                                 <div className="row pt-2">
-                                    <div className="col-md-6 col-xl-3">
+                                    <div className="customCol">
                                         <div className="card">
                                             <div className="card-body" style={{backgroundColor:'#96d172',borderRadius:'20px'}}>
                                                 <div className="widget-first">
@@ -138,7 +138,7 @@ export default function Dashboard({selectedDate,selectedHotel}){
                                         </div>
                                     </div>
 
-                                    <div className="col-md-6 col-xl-3">
+                                    <div className="customCol">
                                         <div className="card">
                                             <div className="card-body" style={{backgroundColor:'#e75656',borderRadius:'20px'}}>
                                                 <div className="widget-first">
@@ -165,7 +165,7 @@ export default function Dashboard({selectedDate,selectedHotel}){
                                         </div>
                                     </div>
 
-                                    <div className="col-md-6 col-xl-3">
+                                    <div className="customCol">
                                         <div className="card">
                                             <div className="card-body" style={{backgroundColor:'#47b0eb',borderRadius:'20px'}}>
                                                 <div className="widget-first">
@@ -192,7 +192,7 @@ export default function Dashboard({selectedDate,selectedHotel}){
                                         </div>
                                     </div>
 
-                                    <div className="col-md-6 col-xl-3">
+                                    <div className="customCol">
                                         <div className="card">
                                             <div className="card-body" style={{backgroundColor:'#e4a74c',borderRadius:'20px'}}>
                                                 <div className="widget-first">
@@ -218,6 +218,35 @@ export default function Dashboard({selectedDate,selectedHotel}){
                                             </div>
                                         </div>
                                     </div>
+    
+                                     <div className="customCol">
+                                        <div className="card">
+                                            <div className="card-body" style={{backgroundColor:'#47b0eb',borderRadius:'20px'}}>
+                                                <div className="widget-first">
+                                                    <div className="d-flex justify-content-between align-items-end pb-5">
+                                                        <div>
+                                                            <div className="d-flex align-items-center mb-3">    
+                                                                <p className="mb-0 text-white fs-15">No Show</p>
+                                                            </div>
+                                                        </div>
+
+                                                        <div>
+                                                            <div id="new-orders" className="apex-charts"></div>
+                                                        </div>
+                                                    </div>
+
+                                                    <div>
+                                                    <p className="text-muted mb-0 fs-13 d-flex justify-content-between" style={{ margin: 0, alignItems: 'flex-end' }}>
+                                                        <span className="text-white fs-14" style={{fontSize: '14px',lineHeight: '1',}}>View Booking Status</span>
+                                                        <span className="text-white" style={{fontSize: '80px',lineHeight: '1', fontWeight:'100'}}>{roomAvailability.availableRoomsCount}</span>
+                                                    </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+    
+    
                                 </div>
                                 </div>
                             </div>
